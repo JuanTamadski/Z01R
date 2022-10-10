cd0807e7a95037c653cd577777830280448f6568
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-10-10 08:49:13 +0800

    Update and rename evolution_Z01R.mk to lineage_Z01R.mk

--- lineage_Z01R.mk
+++ lineage_Z01R.mk
@@ -23,14 +23,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Evo stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := evolution_Z01R
-EVO_BUILD_TYPE := OFFICIAL
+PRODUCT_NAME := lineage_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
